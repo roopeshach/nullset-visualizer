af0ece262e2c7578f39243e75d64a7214bbf5721
Roopesh Acharya <rupeshacharya2014@gmail.com>
2023-04-30 15:13:03 +0545

    null visualizer to svg

---
+++ final.py
@@ -0,0 +1,153 @@
+import math
+
+class Circle:
+    """
+    A circle with a radius and a list of other circles placed inside it.
+    Args:
+        radius (float): The radius of the circle.
+    
+    """
+    def __init__(self, radius):
+        self.radius = radius
+        self.contents = []  # [Placement]
+
+    def add(self, circle, pos):
+        """
+        Adds a circle to the list of circles inside this circle.
+        Args:
+            circle (Circle): The circle to add.
+            pos (tuple): The position of the circle relative to this circle.
+        """
+
+        self.contents.append(Placement(circle, pos))
+
+class Placement:
+    """
+    A circle placed inside another circle.
+    Args:
+        circle (Circle): The circle being placed.
+        pos (tuple): The position of the circle relative to the circle it is being placed in.
+    """
+
+    def __init__(self, circle, pos):
+        self.circle = circle
+        self.pos = pos
+
+# make 0 and 1
+cache = {0: Circle(.5), 1: Circle(1.)}
+cache[1].add(cache[0], (-.5, 0))
+
+def make(n):
+    """
+    Makes a circle with n circles inside it.
+    Args:
+        n (int): The number of circles to place inside the circle.
+    Returns:
+        Circle: The circle with n circles inside it.
+    """
+
+    if n in cache:
+        return cache[n]
+
+    a = make(n - 1)
+    b = make(n - 2)
+    z = Circle(a.radius + b.radius)
+    z.add(a, (-b.radius, 0))
+    z.add(b, (a.radius, 0))
+
+    if n - 3 >= 0:
+        c = make(n - 3)
+        p = getnestpos(a, b, c)
+        z.add(c, p)
+
+        nest(z, n - 4, b, a)
+    
+    cache[n] = z
+    return z
+
+def nest(z, n, a, b, recurse=True):
+    """
+    Places a circle inside another circle.
+    Args:
+        z (Circle): The circle to place the circle inside.
+        n (int): The number of circles to place inside the circle.
+        a (Circle): The circle to place inside the other circle.
+        b (Circle): The circle to place the other circle inside.
+        recurse (bool): Whether to call nest recursively.
+    """
+
+    if n >= 0:
+        ap = bp = None
+        for q in z.contents:
+            if q.circle is a:
+                ap = q
+            if q.circle is b:
+                bp = q
+        assert ap and bp, 'failed to find ap and bp'
+
+        c = make(n)
+        p = getnestpos(a, b, c, ap, bp)
+        z.add(c, p)
+
+        if recurse:
+            nest(z, n - 1, a, c, recurse=False)
+            nest(z, n - 2, c, b)
+
+def getnestpos(a, b, c, ap=None, bp=None):
+    """
+    Gets the position of c relative to a.
+    Args:
+        a (Circle): The circle that c is being placed inside.
+        b (Circle): The circle that c is being placed outside of.
+        c (Circle): The circle that is being placed inside a.
+        ap (Placement): The placement of a relative to b.
+        bp (Placement): The placement of b relative to a.
+    Returns:
+        tuple: The position of c relative to a.
+    """
+
+    # use law of cosines to get angle between a->b and a->c
+    p = a.radius ** 2 + a.radius * b.radius + a.radius * c.radius
+    angle = math.acos((p - b.radius * c.radius) / (p + b.radius * c.radius))
+    # find the vector a->c
+    x = (a.radius + c.radius) * math.cos(angle) - b.radius
+    y = (a.radius + c.radius) * math.sin(angle)
+    
+    if ap and bp:
+        # place the vector correctly
+        vx = bp.pos[0] - ap.pos[0]
+        vy = bp.pos[1] - ap.pos[1]
+        angle = math.atan2(vy, vx)
+        vmag = math.hypot(vx, vy)
+        vx -= a.radius * vx / vmag
+        vy -= a.radius * vy / vmag
+        
+        x, y = (
+            ap.pos[0] + vx + x * math.cos(angle) - y * math.sin(angle),
+            ap.pos[1] + vy + x * math.sin(angle) + y * math.cos(angle)
+        )
+        
+    return (x, y)
+
+
+def printsvg(c, pos, file):
+    """
+    Prints a circle to an svg file.
+    Args:
+        c (Circle): The circle to print.
+        pos (tuple): The position of the circle relative to the svg file.
+        file (file): The file to print to.
+    """
+
+    file.write('<circle cx="%f" cy="%f" r="%f" fill="white" stroke="black" />\n' % (zoom * pos[0], zoom * pos[1], zoom * c.radius))
+    for p in c.contents:
+        printsvg(p.circle, (pos[0] + p.pos[0], pos[1] + p.pos[1]), file)
+
+zoom = 20
+c = make(10)
+
+size = zoom * c.radius * 2
+with open('output.svg', 'w') as f:
+    f.write('<svg xmlns="http://www.w3.org/2000/svg" width="%f" height="%f">\n' % (size, size))
+    printsvg(c, (c.radius, c.radius), f)
+    f.write('</svg>')
---
+++ powerset.py
@@ -0,0 +1,116 @@
+#
+import tkinter as tk
+import numpy as np
+import random
+
+def intersect(collection, items):
+  """
+  Adds items to collection if they are not already in collection.
+  """
+  for item in items:
+    if sorted(item) not in collection:
+      collection.append(sorted(item))
+      
+
+def powerset(myset):
+  if not myset:
+    return []
+  # F (e, T) = { X ∪ {e} | X ∈ T }
+  def F(e, myset):
+    intermediary = []
+    
+    for outer in myset:
+      new = [e]
+      for inner in myset:
+        if inner != e:
+          new.append(inner)
+      intermediary.append(new)
+    return intermediary
+# T = S\{e} (where \ means relative complement or subtraction)
+    # P(S) = P(T) ∪ F ( e, P(T))
+  T = []
+  for item in myset:
+      Tgroup = []
+      for inneritem in myset:
+        if inneritem != item:
+          Tgroup.append(inneritem)
+      T.append(Tgroup)
+  sets = []
+  for Tgroup in T:
+      if Tgroup:
+        intersect(sets, [Tgroup])
+  for Tgroup in T:
+    sublist = powerset(Tgroup)
+    for item in myset:
+      intersect(sets, F(item, myset))
+    if sublist:
+      intersect(sets, sublist)
+  return sets
+
+items = powerset(
+  ['r', 'u', 'p', 'e', 's', 'h', 'a', 'c', 'h', 'y']
+)
+results = []
+print(items)
+for item in items:
+  for subitem in item:
+    results.append(subitem)
+print(len(results))
+
+
+
+def draw_circle(brackets):
+    """
+    Draws a circle using Tkinter for each set in the nested list of sets (brackets).
+    """
+    root = tk.Tk()
+    root.title("Circle Drawing")
+    
+    # Get screen size
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Set canvas size to fit screen
+    canvas_width = screen_width
+    canvas_height = screen_height
+    canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    canvas.pack()
+
+    def draw_nested_circle(brackets, x, y, radius):
+      """
+      Helper function to draw a circle and any nested circles inside it.
+      """
+      if isinstance(brackets, list):
+          max_distance = radius
+          num_circles = len(brackets)
+          for i, inner_brackets in enumerate(brackets):
+              angle = (2*i + 1) * (180 / num_circles) - 90
+              dx = radius * 0.8
+              dy = radius * 0.8
+              inner_radius = draw_nested_circle(inner_brackets, x + dx * np.cos(np.radians(angle)), y + dy * np.sin(np.radians(angle)), radius/2)
+              distance = np.sqrt(dx**2 + dy**2) + inner_radius
+              if distance > max_distance:
+                  max_distance = distance
+          color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+          canvas.create_oval(x-max_distance, y-max_distance, x+max_distance, y+max_distance, outline=color)
+          for i, inner_brackets in enumerate(brackets):
+              angle = (2*i + 1) * (180 / num_circles) - 90
+              dx = max_distance * 0.8
+              dy = max_distance * 0.8
+              draw_nested_circle(inner_brackets, x + dx * np.cos(np.radians(angle)), y + dy * np.sin(np.radians(angle)), radius/2)
+      else:
+          color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+          canvas.create_oval(x-radius, y-radius, x+radius, y+radius, outline=color)
+      return radius
+
+
+    # Draw main circle in center of screen
+    main_radius = 200
+    main_x = screen_width / 2
+    main_y = screen_height / 2
+    draw_nested_circle(brackets, main_x, main_y, main_radius)
+
+    root.mainloop()
+
+
+draw_circle(items)
